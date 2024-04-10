9301f44c05c007d7977b0a51e4f32746b9d9e2df
79244 <dumichkira05@gmail.com>
2024-04-10 20:21:04 +1000

    Добавлено администрирование модели Версий

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from catalog.models import Category, Product
+from catalog.models import Category, Product, Version
+
 
 # admin.site.register(Category)
 # admin.site.register(Product)
@@ -15,5 +16,12 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'cost', 'category')
     list_filter = ('category', 'name')
     search_fields = ('name', 'description')
+
+
+@admin.register(Version)
+class VersionAdmin(admin.ModelAdmin):
+    list_display = ('product', 'title', 'number', 'feature')
+    list_filter = ('number', 'feature')
+    search_fields = ('number', 'feature')
 
 
