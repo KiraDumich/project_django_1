6f5ffe01ab92670fb43ab04cd2faf2947062a884
79244 <dumichkira05@gmail.com>
2024-04-27 22:49:07 +1000

    Прочие доработки

--- users/models.py
+++ users/models.py
@@ -13,7 +13,7 @@ class User(AbstractUser):
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
     country = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
     verified = models.BooleanField(default=False, verbose_name='верифицирован', blank=True)
-    verified_password = models.IntegerField(verbose_name='ключ для верификации', **NULLABLE)
+    verified_password = models.CharField(verbose_name='ключ для верификации', **NULLABLE, max_length=20)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
