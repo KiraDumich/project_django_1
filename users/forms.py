bf11ae96072df2d10f423428639dc5440f3622f3
79244 <dumichkira05@gmail.com>
2024-05-22 21:33:24 +1000

    Поправки

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,16 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordResetForm
 
-from catalog.forms import StyleFormMixin
 from users.models import User
 from django import forms
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from users.models import User
+
+
+class StyleFormMixin():
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
 
 
 class RegistrationForm(StyleFormMixin, UserCreationForm):
